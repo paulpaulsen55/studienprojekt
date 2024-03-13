4e7bf96991ac95bf8097cb8b73fec4a70b3fde2f
paulpaulsen55 <paulpaulsenx@gmail.com>
2024-03-13 23:35:29 +0100

    added templating

--- public/index.php
+++ public/index.php
@@ -2,14 +2,19 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$twig = Twig::create('../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
 });
 
 $app->run();
