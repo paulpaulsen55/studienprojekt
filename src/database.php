c468d884871eb0ec3641586765f84567bdf0535b
Justus <justus.winter@urbanandmainlines.com>
2024-03-15 14:33:45 +0100

    fixed database class

--- src/database.php
+++ src/database.php
@@ -9,14 +9,22 @@ class Database {
         $user = 'root';
         $pass = '';
         $charset = 'utf8mb4';
-
         $dsn = "mysql:host=$host;dbname=$dbName;charset=$charset";
         $opt = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
-        $this->pdo = new PDO($dsn, $user, $pass, $opt);
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $opt);
+        } catch (PDOException $e) {
+            if ($e->getCode() == 1049) {
+                $this->pdo = new PDO("mysql:host=$host;", $user, $pass, $opt);
+                $this->pdo->exec("CREATE DATABASE `$dbName`;USE `$dbName`;");
+            } else {
+                throw $e;
+            }
+        }
     }
 
     public static function getInstance($dbName) {
@@ -28,5 +36,30 @@ class Database {
 
     public function getPdo() {
         return $this->pdo;
+    }
+
+    private function createDatabase($dbName) {
+        $stmt = $this->pdo->prepare($sql, "SHOW DATABASES LIKE :dbName");
+        $stmt->execute(['dbName' => $dbName]);
+        if ($stmt->fetch(PDO::FETCH_ASSOC) === false) {
+            $this->pdo->exec("CREATE DATABASE {$dbName}");
+        }
+    }
+
+    public function createTable($tableName, $columns) {
+        $columnsSql = implode(', ', array_map(function($column, $type) {
+            return "$column $type";
+        }, array_keys($columns), $columns));
+
+        $sql = "CREATE TABLE IF NOT EXISTS $tableName ($columnsSql)";
+        $this->pdo->exec($sql);
+    }
+
+    public function insertData($tableName, $data) {
+        $columns = implode(', ', array_keys($data));
+        $placeholders = implode(', ', array_fill(0, count($data), '?'));
+        $sql = "INSERT INTO $tableName ($columns) VALUES ($placeholders)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(array_values($data));
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -5,12 +5,24 @@ use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
+require __DIR__ . '/../database.php';
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $twig = Twig::create('../templates', ['cache' => false]);
 $app->add(TwigMiddleware::create($app, $twig));
+
+
+$db = Database::getInstance('bank');
+
+$db->createTable('users', [
+    'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+    'name' => 'VARCHAR(100)',
+    'balance' => 'DECIMAL(10, 2) DEFAULT 0'	
+]);
+
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
@@ -33,6 +45,8 @@ $app->get('/t2', function (Request $request, Response $response, $args) {
 });
 
 $app->get('/t3', function (Request $request, Response $response, $args) {
+    $db1 = Database::getInstance('test_db1')->getPdo();
+
     $view = Twig::fromRequest($request);
     return $view->render($response, 't3.twig');
 });
